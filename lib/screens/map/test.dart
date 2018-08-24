18da5fb4f26fa64c969a56f259446c5cab58e3f6
yimankaing <yimankaing@gmail.com>
2018-08-24 22:16:08 +0700

    finish rest-api and listViewBuilder

--- lib/screens/map/test.dart
+++ lib/screens/map/test.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 
@@ -8,16 +10,31 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  List<Map<String, dynamic>> items = List.generate(
-    20,
-    (i) => {"name": "name${i}", "gender": "male", "address": "btb"},
+  // List<SubjectItem> _subjects = [];
+  List<SubjectItem> _subjects = List.generate(
+    25,
+    (i) => SubjectItem(id: '${i+1}', khName: 'yiman', enName: 'male'),
   );
+  Future fetchSubject(String params) {
+    String url = 'http://192.168.43.3:3000/find-subject/${params}';
+    var httpClient = http.Client();
+    return httpClient.get(url).then((response) {
+      var result = json.decode(response.body);
+
+      result.map((o) => _subjects.add(SubjectItem.fromMap(o))).toList();
+      setState(() {
+        _subjects;
+      });
+    });
+  }
+
   ScrollController _scrollController = ScrollController();
   bool isPerformingRequest = false;
 
   @override
   void initState() {
     super.initState();
+    // fetchSubject("{}");
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
@@ -38,30 +55,33 @@ class _TestState extends State<Test> {
       appBar: AppBar(
         title: Text("ListView builder"),
       ),
-      body: ListView.builder(
-        itemCount: items.length + 1,
-        itemBuilder: (context, index) {
-          if (index == items.length) {
-            return _buildProgressIndicator();
-          } else {
-            return _listItem(items[index], index);
-          }
-        },
-        controller: _scrollController,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _subjects.length,
+              padding: const EdgeInsets.only(bottom: 30.0),
+              itemBuilder: (context, index) {
+                final SubjectItem subjectItem = _subjects[index];
+                return _listItem(subjectItem, index);
+              },
+              controller: _scrollController,
+            ),
+          ),
+          isPerformingRequest ? _buildProgressIndicator() : Container()
+        ],
       ),
     );
   }
 
-  Widget _listItem(item, i) {
+  Widget _listItem(SubjectItem item, int i) {
     return ListTile(
       leading: Icon(Icons.book),
-      title: Text(item['name']),
-      subtitle: Text("${i}"),
-      // onTap: () {
-      //   setState(() {
-      //     items.removeAt(i);
-      //   });
-      // },
+      title: Text(item.khName.toString() + item.id.toString()),
+      subtitle: Text(item.enName.toString()),
+      onTap: () {
+        // fetchSubject(json.encode(item.toMap()));
+      },
       trailing: IconButton(
         icon: Icon(
           Icons.delete,
@@ -69,7 +89,7 @@ class _TestState extends State<Test> {
         ),
         onPressed: () {
           setState(() {
-            items.removeAt(i);
+            _subjects.removeAt(i);
           });
         },
       ),
@@ -78,11 +98,13 @@ class _TestState extends State<Test> {
 
   Widget _buildProgressIndicator() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(5.0),
       child: Center(
         child: Opacity(
           opacity: isPerformingRequest ? 1.0 : 0.0,
-          child: CircularProgressIndicator(strokeWidth: 2.3,),
+          child: CircularProgressIndicator(
+            strokeWidth: 4.3,
+          ),
         ),
       ),
     );
@@ -91,7 +113,8 @@ class _TestState extends State<Test> {
   _loadMoreData() async {
     if (!isPerformingRequest) {
       setState(() => isPerformingRequest = true);
-      List<Map<String, dynamic>> newEntries = await fakeRequest();
+      List<SubjectItem> newEntries = await fakeRequest();
+
       if (newEntries.isEmpty) {
         double edge = 50.0;
         double offsetFromBottom = _scrollController.position.maxScrollExtent -
@@ -103,19 +126,44 @@ class _TestState extends State<Test> {
               curve: Curves.easeOut);
         }
       }
+
       setState(() {
-        items.addAll(newEntries);
+        _subjects.addAll(newEntries);
         isPerformingRequest = false;
       });
     }
   }
 
-  Future<List<Map<String, dynamic>>> fakeRequest() async {
+  Future<List<SubjectItem>> fakeRequest() async {
     return Future.delayed(Duration(seconds: 1), () {
-      var arr = [
-        {"name": "nickname", "gender": "female", "address": "pp"}
-      ];
-      return arr.toList();
+      // return [
+      //   SubjectItem(id: "0099", khName: "veasna", enName: "male"),
+      //   SubjectItem(id: "0100", khName: "veasna1", enName: "male1"),
+      //   SubjectItem(id: "0101", khName: "veasna12", enName: "male12"),
+      // ];
+      return [];
     });
+  }
+}
+
+class SubjectItem {
+  JsonDecoder jsonDecoder = new JsonDecoder();
+  String id;
+  String khName;
+  String enName;
+
+  SubjectItem({this.id, this.khName, this.enName});
+
+  SubjectItem.fromMap(Map<String, dynamic> map)
+      : khName = map['khName'] as String,
+        enName = map['enName'] as String,
+        id = map['_id'] as String;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map['khName'] = khName;
+    map['enName'] = enName;
+    map['_id'] = id;
+    return map;
   }
 }
